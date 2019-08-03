25b5ba19d34170deb9aa73bbd13888b12a759c04
Oskar Kunik <oskar.kunik@gmail.com>
2019-08-03 20:55:12 +0200

    1.5: course information, step5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Header = (props) => (
-    <h1>{props.course}</h1>
+    <h1>{props.name}</h1>
 )
 
 const Part = (props) => (
@@ -34,30 +34,33 @@ const Total = (props) => (
 
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ]
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
+
 
     return (
         <div>
-            <Header course={course} />
+            <Header name={course.name} />
             <Content
-                parts={parts}        
+                parts={course.parts}        
             />
             <Total
-                parts={parts}        
+                parts={course.parts}        
             />
         </div>
     )
