8e0fde55aca05f786a1cb849fdc939940ac93bfb
Oskar Kunik <oskar.kunik@gmail.com>
2019-08-18 11:20:46 +0200

    2.1: course contents step6

---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => (
+    <h1>{props.name}</h1>
+)
+
+const Part = (props) => (
+    <p>
+        {props.name} {props.exercises}
+    </p>
+)
+
+const Content = ({parts}) => {
+    const rows = () => parts.map(part =>
+        <Part
+            name={part.name}
+            exercises={part.exercises}
+            key={part.id}
+        />
+    )
+    return (
+        <>
+            {rows()}
+        </>
+    )
+}
+
+const Total = (props) => (
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+)
+
+const Course = ({course}) => {
+    return (
+        <div>
+           <Header name={course.name} />
+            <Content
+                 parts={course.parts}        
+            />
+        </div>
+    )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
