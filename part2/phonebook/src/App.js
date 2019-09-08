e0c0fe5a9eb20388e6626c6535d7c0bd3d66869e
Oskar Kunik <oskar.kunik@gmail.com>
2019-09-08 12:35:34 +0200

    2.15: Phonebook step7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -28,9 +28,13 @@ const App = () => {
       number: newNumber,
     }
 
-    setPersons(persons.concat(newPersonObject))
-    setNewName('')
-    setNewNumber('')
+    axios.post('http://localhost:3001/persons', newPersonObject)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
+
   }
 
   const handleNameChange = (event) => {
